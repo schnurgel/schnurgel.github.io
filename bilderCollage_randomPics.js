d1f4e674049c6ac77b5fcda6de15088299c1aeac
Natalie Schmitzberger <natalieschmitzberger@gmail.com>
2022-01-18 14:26:08 +0100

    edit

--- bilderCollage_randomPics.js
+++ bilderCollage_randomPics.js
@@ -5,5 +5,5 @@ for (let i = 0; i < 15; i++) {
     if(i%3==0){
         contentBild.innerHTML += `<br>`;    
     }
-    contentBild.innerHTML += `<img src="https://picsum.photos/500?random=${i}" alt="galeriebild"></img>`;    
+    contentBild.innerHTML += `<img src="bild${i}.jpg" alt="galeriebild"></img>`;    
 }
